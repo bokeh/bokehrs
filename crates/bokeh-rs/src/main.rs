09ee934af8d153576ea8a469cb1e06404304438b
Mateusz Paprocki <mattpap@gmail.com>
2023-06-10 15:23:56 +0200

    Experiment with HTTP servers

--- crates/bokeh-rs/src/main.rs
+++ crates/bokeh-rs/src/main.rs
@@ -1,19 +1,41 @@
-use polars::lazy::prelude::*;
-use polars::prelude::*;
-
+/*
 struct Resources;
 struct Document;
 
+trait Layoutable {
+}
+
+struct Row {
+  children: Vec<Box<dyn Layoutable>>,
+}
+struct Column;
+struct Grid;
+struct Plot;
+struct GridPlot;
+*/
+
+/*
 fn main() {
-  let df = df! [
-    "A"        => [1, 2, 3, 4, 5],
-    "fruits"   => ["banana", "banana", "apple", "apple", "banana"],
-    "B"        => [5, 4, 3, 2, 1],
-    "cars"     => ["beetle", "audi", "beetle", "beetle", "beetle"],
-    "optional" => [Some(28), Some(300), None, Some(2), Some(-30)],
-  ].unwrap();
-
-  df.lazy().select([col("A")]);
-
   println!("Bokeh");
+}
+*/
+
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+  //pretty_env_logger::init();
+
+  // GET /hello/warp => 200 OK with body "Hello, warp!"
+  let hello = warp::path!("hello" / String)
+      .map(|name| format!("Hello, {}!\n", name));
+
+  //let log = warp::log("bokeh-rs");
+  let log = warp::any().map(|| { println!("foo"); warp::reply() });
+
+  let routes = hello.or(log);
+
+  warp::serve(routes)
+    .run(([127, 0, 0, 1], 8000))
+    .await;
 }
